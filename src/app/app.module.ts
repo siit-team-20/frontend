612d01f9b79c21e9c2640ed510ded75f9954fe9a
Andro <andrija.slovic1@gmail.com>
2023-12-18 20:07:00 +0100

    fixed merging errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AccommodationCreateComponent } from './accommodation-create/accommodation-create.component';
+import { AccommodationCreateComponent } from './accommodation/accommodation-create/accommodation-create.component';
 import { NavbarModule } from './navbar/navbar.module';
 import { AccommodationRequestModule } from './accommodation-request/accommodation-request.module';
 
@@ -20,7 +18,7 @@ import { AccommodationRequestModule } from './accommodation-request/accommodatio
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    AccommodationCreateComponent
+    AccommodationCreateComponent,
     NavbarModule,
     AccommodationRequestModule
   ],
