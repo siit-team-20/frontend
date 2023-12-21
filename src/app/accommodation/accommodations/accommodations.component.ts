b83cefcf51edeecdad70cd0642f4650996f53e1f
MVojkan <mvojkan02.7@gmail.com>
2023-12-21 10:36:21 +0100

    Fixed all bugs in filter accommodations

--- src/app/accommodation/accommodations/accommodations.component.ts
+++ src/app/accommodation/accommodations/accommodations.component.ts
@@ -32,7 +32,7 @@ export class AccommodationsComponent {
 
 
   searchtext: string  = '';
-  searchnumber: string = '';
+  searchnumber: number = 0;
   searchprice: string ='';
   searchtype: string =''; 
   searchbenefits: any[] = [""];
