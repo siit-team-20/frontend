56347920c74328429263cba4cf27f1c4ad4577d6
MVojkan <mvojkan02.7@gmail.com>
2024-01-21 17:27:40 +0100

    added accommodation favourites

---
+++ src/app/accommodation/accommodation-favourites/accommodation-favourites.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { AxiosService } from '../../axios.service';
+import { Router } from '@angular/router';
+import { Accommodation } from '../model/accommodation';
+import { UserType } from '../../auth/model/user';
+import { CommonModule } from '@angular/common';
+import { AccommodationViewComponent } from '../accommodation-view/accommodation-view.component';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-accommodation-favourites',
+  templateUrl: './accommodation-favourites.component.html',
+  styleUrl: './accommodation-favourites.component.css',
+  standalone: true,
+  imports: [CommonModule, AccommodationViewComponent, FormsModule]
+})
+export class AccommodationFavouritesComponent {
+
+  accommodations: Accommodation[] = [];
+  constructor(private axiosService: AxiosService, private router: Router){
+
+
+   
+  }
+
+  ngOnInit(): void {
+
+    let query: string = "";
+    if (this.axiosService.getRole() == UserType.Guest)
+      query = "?guestEmail=" + this.axiosService.getUser()["sub"];
+
+    this.axiosService.request(
+      "GET",
+      "/api/accommodations/favourites" + query,
+      {}
+    ).then(
+      response => {
+        this.accommodations = response.data;
+        console.log(this.accommodations)
+      });
+  }
+
+}
